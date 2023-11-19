0adff3f7b7ac34842c93491591c9f8d67ba62d50
Timmy24256 <jjgonzasa05012004@gmail.com>
2023-11-19 18:52:23 -0500

    Modelo Cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace ParcialRostrosFelices.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string TipoDocumento { get; set; }
+        public int NroDocumento { get; set; }
+        public int Telefono { get; set; }
+    }
+}
